f5d0206b4b2ba24d2abcbbf05c4044ad2cb9876c
Lisa Mbuwa <mbuwalisa@gmail.com>
2023-07-31 16:30:33 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// const obj  = {
+//     greeting: "Hello , world!",
+//     sayHello: function() {
+//         console.log("this.greeting");
+//     }
+// }
+
+// module.exports = obj;
+const os = require ("os")
+console.log(os.freemem())
+console.log(os.totalmem())
+console.log(os.hostname())
+console.log(os.platform())
+console.log(os.userInfo())
+
